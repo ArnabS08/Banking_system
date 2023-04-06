c00420995899a312d86e781951397844d4473d8e
ArnabSO8 <arnab.shrestha08@gmail.com>
2023-04-05 19:55:32 -0500

    New feat

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Welcome to Monkey Bank!")
+
+while True:
+    log_or_sign = input("Login in or sign up? ")
+
+    if log_or_sign.lower() == "login in":
+        account_num = input("Acount number: ")
+        pin = input("PIN: ")
+        break
+
+    elif log_or_sign.lower() == "sign up":
+        first_name = input("First name: ")
+        last_name = input("Last name: ")
+        date_of_birth = input("Date of birth (##/##/####): ")
+        print(f"New user has been created.\nWelcome {first_name} {last_name}!")
+        break
+
+    else:
+        print("You can only type login in, or sign up.")
