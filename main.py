9653fd0f4168e7f1b3c95b6c0afc36cc36a436e9
ArnabSO8 <arnab.shrestha08@gmail.com>
2023-04-12 21:25:29 -0500

    Implementing database in code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import mysql.connector
 
 connection = mysql.connector.connect(
@@ -7,8 +9,8 @@ connection = mysql.connector.connect(
 )
 
 cursor = connection.cursor()
+addData = ("INSERT INTO data (account_num, pin, first_name, last_name, birth_date, balance) VALUES (%s, %s, %s, %s, %s, %s)")
 testQuery = ("SELECT * FROM data")
-cursor.execute(testQuery)
 
 print("Welcome to Monkey Bank!")
 
@@ -23,9 +25,22 @@ while True:
     elif log_or_sign.lower() == "sign up":
         first_name = input("First name: ")
         last_name = input("Last name: ")
-        date_of_birth = input("Date of birth (##/##/####): ")
-        print(f"New user has been created.\nWelcome {first_name} {last_name}!")
-        break
+        birth_date = input("Date of birth (YYYY/MM/DD): ")
+        pin = input("Create PIN: ")
+        account_num = random.randint(1,10000000000)
+        while account_num == cursor.execute("SELECT account_num * FROM data"):
+            account_num == random.randint(1,10000000000)
+
+        data = (account_num, int(pin), first_name, last_name, birth_date) # List of new values
+        try:
+            cursor.execute(addData, data) # This adds the new values 
+            connection.commit() # This commits the new data.
+
+            print(f"New user has been created.\nWelcome {first_name} {last_name}!")
+            break
+        except:
+            connection.rollback() # If an error occured while commiting, this reverses the commit.
+            print("There was a problem while creating your account, please try again.")
 
     else:
         print("You can only type login in, or sign up.")
